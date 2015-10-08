757a2b76c9af194bee34de041fbb12f6d5231d3e
Feiji <xiaofei.zhang1988@gmail.com>
2015-10-08 10:13:06 -0700

    Connected with phone

--- EstimotePlay/MainActivity.cs
+++ EstimotePlay/MainActivity.cs
@@ -3,20 +3,40 @@
 using Android.App;
 using Android.Content;
 using Android.Runtime;
+using Android.Util;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using EstimoteSdk;
+
+using Java.Util.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+using JavaObject = Java.Lang.Object;
 
 namespace EstimotePlay
 {
-	[Activity (Label = "EstimotePlay", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
+	[Activity (Label = "Estimote Play", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity, BeaconManager.IServiceReadyCallback, BeaconManager.IRangingListener
 	{
+		static readonly string Tag = typeof(MainActivity).FullName;
+		static readonly Region ALL_ESTIMOTE_BEACONS = new Region("rid", "B9407F30-F5F8-466E-AFF9-25556B57FE6D", null, null);
+
+		private Dictionary<string, double> Dis = new Dictionary<string, double>();
+		private Dictionary<string, string> MacToName = new Dictionary<string, string>();
+
 		int count = 1;
+
+		BeaconManager _beaconManager;
 
 		protected override void OnCreate (Bundle bundle)
 		{
 			base.OnCreate (bundle);
+
+			this.MacToName ["F7:B7:89:83:A4:1D"] = "Blueberry";
+			this.MacToName ["D2:F5:46:E5:00:39"] = "Mint";
+			this.MacToName ["F5:9E:47:57:24:AA"] = "Ice";
 
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
@@ -28,6 +48,55 @@ namespace EstimotePlay
 			button.Click += delegate {
 				button.Text = string.Format ("{0} clicks!", count++);
 			};
+
+			TextView text = FindViewById<TextView> (Resource.Id.textView1);
+
+			_beaconManager = new BeaconManager (this);
+			_beaconManager.SetRangingListener (this);
+
+			_beaconManager.Connect (this);
+
+			/*_findBeacon = new FindSpecificBeacon(this);
+			_findBeacon.BeaconFound += (sender, e) => {
+				Log.Debug(Tag, "Found the beacon!");
+			};*/
+		}
+
+		public void OnBeaconsDiscovered (Region region, IList<Beacon> beacons)
+		{
+			Log.Debug (Tag, "Ranged beacons: " + beacons.Count);
+			if (beacons.Count != 0) 
+			{
+				foreach (var b in beacons) {
+					var distance = Utils.ComputeAccuracy (b);
+					Log.Debug ("BeaconDetails:", b.MacAddress + " distance: " + distance);
+					Dis [b.MacAddress] = distance;
+				}
+			}
+
+			var text = string.Empty;
+			var keys = Dis.Keys.ToList ();
+			keys.Sort ();
+
+			foreach (var k in keys) 
+			{
+				text += MacToName[k] + " " + Dis [k] + "\n";
+			}
+
+			FindViewById<TextView> (Resource.Id.textView1).Text = text;
+		}
+
+		public void OnServiceReady()
+		{
+			// This method is called when BeaconManager is up and running.
+			_beaconManager.StartRanging(ALL_ESTIMOTE_BEACONS);
+		}
+
+		protected override void OnDestroy()
+		{
+			// Make sure we disconnect from the Estimote.
+			_beaconManager.Disconnect ();
+			base.OnDestroy ();
 		}
 	}
 }
--- EstimotePlay/Resources/Resource.designer.cs
+++ EstimotePlay/Resources/Resource.designer.cs
@@ -47,6 +47,9 @@ namespace EstimotePlay
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int monoandroidsplash = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -60,8 +63,11 @@ namespace EstimotePlay
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int myButton = 2131099648;
+			
+			// aapt resource value: 0x7f060001
+			public const int textView1 = 2131099649;
 			
 			static Id()
 			{
@@ -104,6 +110,22 @@ namespace EstimotePlay
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int Mono_Android_Theme_Splash = 2131034112;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
